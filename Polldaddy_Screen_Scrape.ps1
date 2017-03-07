a45cd64228f18f13bf22f3eb99d998141d2756eb
itsalwayssonny <ssmith@juggle.guru>
2017-03-07 12:23:27 -0600

    Now writing to a single CSV
    Getting smarter every day :-)

--- Polldaddy_Screen_Scrape.ps1
+++ Polldaddy_Screen_Scrape.ps1
@@ -1,14 +1,12 @@
-#Invoke-WebRequest http://polls.polldaddy.com/vote-js.php?p=9317003
-#$ResultsPage = [io.file]::WriteAllBytes($tempfilename,(Invoke-WebRequest -URI "http://polls.polldaddy.com/vote-js.php?p=9317003").content)
-$datetime = Get-Date -format "yyyyMMdd_HH.mm.ss"
-$ShortTime = Get-Date -format "HH:mm"
-$scrapelog = "D:\NOLA\Samples\scrape1a_" + $datetime + ".log"
-$scrapesite = Invoke-WebRequest http://polls.polldaddy.com/vote-js.php?p=9317003
-#$scrapesite.AllElements | Where {$_.innerText -match '(^2016 Big Idea '} | Out-File $scrapelog
-#$scrapesite.AllElements | Out-File $scrapelog
+#Polldaddy Screen Scraper v2.0
+$datetime = Get-Date -format "yyyy/MM/dd HH:mm:ss"
+#Just wrote a reminder here to avoide editing a LIVE file. Copy CSV before messing with it.
+$scrapelog = "D:\NOLA\2017\ScrapeSample_DO_NOT_EDIT.csv"
+$scrapesite = Invoke-WebRequest http://polls.polldaddy.com/vote-js.php?p=9679392
 $scrapeArrayNames = $scrapesite.AllElements | Where Class -eq "pds-answer-text" | Select -First 20 -ExpandProperty innerText
 $scrapeArrayVotes = $scrapesite.AllElements | Where Class -eq "pds-feedback-votes" | Select -First 20 -ExpandProperty innerText | ForEach {$_ -replace '(\D)'}
 $scrapeCRUD = For ($i = 0; $i -le 19; $i++) {$scrapeArrayNames[$i] + "votes=" + $scrapeArrayVotes[$i]}
-"TimeCode: " + $datetime + "`r`n" + $ShortTime | Out-File $scrapelog
-$scrapeCRUD | Out-File $scrapelog -Append
+#Prepend Array with Date and time so this is all one file now.
+$scrapeCSV = ,$datetime + $scrapeCRUD
+$scrapeCSV -join "," | Out-File $scrapelog -Append
 exit
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Polldaddy-Screen-Scrape
+v1.0
+It pulls data and writes to separate log files.
+
+v2.0
+Script writes to a CSV for easy port to data visualizers.
